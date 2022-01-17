36e177fb5c8a97de18e9b5b621b4b59acf87d504
Agung Prakoso <72962612+ApJr18@users.noreply.github.com>
2022-01-17 14:12:45 +0700

    Update edit.php
    Agung Prakoso_19SA3052

--- rest_ci_client_mahasiswa/application/views/mahasiswa/edit.php
+++ rest_ci_client_mahasiswa/application/views/mahasiswa/edit.php
@@ -1,24 +1,15 @@
-<?php echo form_open('kontak/edit'); ?>
-<?php echo form_hidden('id', $datakontak[0]->id); ?>
+<?php echo form_open('mahasiswa/edit');?>
+<?php echo form_hidden('id',$datamahasiswa[0]->id);?>
 
 <table>
-    <tr>
-        <td>ID</td>
-        <td><?php echo form_input('', $datakontak[0]->id, "disabled"); ?></td>
-    </tr>
-    <tr>
-        <td>NAMA</td>
-        <td><?php echo form_input('nama', $datakontak[0]->nama); ?></td>
-    </tr>
-    <tr>
-        <td>NOMOR</td>
-        <td><?php echo form_input('nomor', $datakontak[0]->nomor); ?></td>
-    </tr>
-    <tr>
-        <td colspan="2">
-            <?php echo form_submit('submit', 'Simpan'); ?>
-            <?php echo anchor('kontak', 'Kembali'); ?></td>
-    </tr>
+    <tr><td>ID</td><td><?php echo form_input('',$datamahasiswa[0]->id,"disabled");?></td></tr>
+    <tr><td>NIM</td><td><?php echo form_input('nim',$datamahasiswa[0]->nim);?></td></tr>
+    <tr><td>NAMA</td><td><?php echo form_input('nama',$datamahasiswa[0]->nama);?></td></tr>
+    <tr><td>NOMOR</td><td><?php echo form_input('nomor',$datamahasiswa[0]->nomor);?></td></tr>
+    <tr><td>ALAMAT</td><td><?php echo form_input('alamat',$datamahasiswa[0]->alamat);?></td></tr>
+    <tr><td colspan="2">
+        <?php echo form_submit('submit','Simpan');?>
+        <?php echo anchor('mahasiswa','Kembali');?></td></tr>
 </table>
 <?php
 echo form_close();
