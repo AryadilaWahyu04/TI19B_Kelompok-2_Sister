0ebdadd68f6ce52a1737cb8e0af552d162838d72
Agung Prakoso <72962612+ApJr18@users.noreply.github.com>
2022-01-17 14:17:06 +0700

    Update create.php
    Agung Prakoso_19SA3052

--- rest_ci_client_mahasiswa/application/views/mahasiswa/create.php
+++ rest_ci_client_mahasiswa/application/views/mahasiswa/create.php
@@ -1,1 +1,13 @@
-
+<?php echo form_open_multipart('mahasiswa/create');?>
+<table>
+    <tr><td>NIM</td><td><?php echo form_input('nim');?></td></tr>
+    <tr><td>NAMA</td><td><?php echo form_input('nama');?></td></tr>
+    <tr><td>NOMOR</td><td><?php echo form_input('nomor');?></td></tr>
+    <tr><td>ALAMAT</td><td><?php echo form_input('alamat');?></td></tr>    
+    <tr><td colspan="2">
+        <?php echo form_submit('submit','Simpan');?>
+        <?php echo anchor('mahasiswa','Kembali');?></td></tr>
+</table>
+<?php
+echo form_close();
+?>
