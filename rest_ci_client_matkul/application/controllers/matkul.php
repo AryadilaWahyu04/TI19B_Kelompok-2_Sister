0d9d25f0189d891037ce737913b8978ad5304dfc
Muhammad Ma'rifatul Izam <70956635+iztea@users.noreply.github.com>
2022-01-17 18:26:41 +0700

    Update matkul.php

--- rest_ci_client_matkul/application/controllers/matkul.php
+++ rest_ci_client_matkul/application/controllers/matkul.php
@@ -1,83 +1,85 @@
 <?php
-class Kontak extends CI_Controller
-{
-
-    var $API = "";
-
-    function __construct()
-    {
+Class matkul extends CI_Controller{
+    
+    var $API ="";
+    
+    function __construct() {
         parent::__construct();
-        $this->API = "http://localhost/rest_ci/index.php/kontak";
+        $this->API="http://localhost/rest_ci/index.php/matkul";
         $this->load->library('session');
         $this->load->library('curl');
         $this->load->helper('form');
         $this->load->helper('url');
     }
-
-    // menampilkan data kontak
-    function index()
-    {
-        $data['datakontak'] = json_decode($this->curl->simple_get($this->API . '/kontak'));
-        $this->load->view('kontak/list', $data);
+    
+    // menampilkan data matkul
+    function index(){
+        $data['datamatkul'] = json_decode($this->curl->simple_get($this->API.'/matkul'));
+        $this->load->view('matkul/list',$data);
     }
-
-    // insert data kontak
-    function create()
-    {
-        if (isset($_POST['submit'])) {
+    
+    // insert data matkul
+    function create(){
+        if(isset($_POST['submit'])){
             $data = array(
-                'id'       =>  $this->input->post('id'),
-                'nama'      =>  $this->input->post('nama'),
-                'nomor' =>  $this->input->post('nomor')
-            );
-            $insert =  $this->curl->simple_post($this->API . '/kontak', $data, array(CURLOPT_BUFFERSIZE => 10));
-            if ($insert) {
-                $this->session->set_flashdata('hasil', 'Insert Data Berhasil');
-            } else {
-                $this->session->set_flashdata('hasil', 'Insert Data Gagal');
+                'id'                =>  $this->input->post('id'),
+                'kd_matkul'         =>  $this->input->post('kd_matkul'),
+                'nama'              =>  $this->input->post('nama'),
+                'dosen'             =>  $this->input->post('dosen'),
+                'kelas'             =>  $this->input->post('kelas'));
+            $insert =  $this->curl->simple_post($this->API.'/matkul', $data, array(CURLOPT_BUFFERSIZE => 10)); 
+            if($insert)
+            {
+                $this->session->set_flashdata('hasil','Insert Data Berhasil');
+            }else
+            {
+               $this->session->set_flashdata('hasil','Insert Data Gagal');
             }
-            redirect('kontak');
-        } else {
-            $this->load->view('kontak/create');
+            redirect('matkul');
+        }else{
+            $this->load->view('matkul/create');
         }
     }
-
-    // edit data kontak
-    function edit()
-    {
-        if (isset($_POST['submit'])) {
+    
+    // edit data matkul
+    function edit(){
+        if(isset($_POST['submit'])){
             $data = array(
-                'id'       =>  $this->input->post('id'),
-                'nama'      =>  $this->input->post('nama'),
-                'nomor' =>  $this->input->post('nomor')
-            );
-            $update =  $this->curl->simple_put($this->API . '/kontak', $data, array(CURLOPT_BUFFERSIZE => 10));
-            if ($update) {
-                $this->session->set_flashdata('hasil', 'Update Data Berhasil');
-            } else {
-                $this->session->set_flashdata('hasil', 'Update Data Gagal');
+                'id'                =>  $this->input->post('id'),
+                'kd_matkul'         =>  $this->input->post('kd_matkul'),
+                'nama'              =>  $this->input->post('nama'),
+                'dosen'             =>  $this->input->post('dosen'),
+                'kelas'             =>  $this->input->post('kelas'));
+            $update =  $this->curl->simple_put($this->API.'/matkul', $data, array(CURLOPT_BUFFERSIZE => 10)); 
+            if($update)
+            {
+                $this->session->set_flashdata('hasil','Update Data Berhasil');
+            }else
+            {
+               $this->session->set_flashdata('hasil','Update Data Gagal');
             }
-            redirect('kontak');
-        } else {
-            $params = array('id' =>  $this->uri->segment(3));
-            $data['datakontak'] = json_decode($this->curl->simple_get($this->API . '/kontak', $params));
-            $this->load->view('kontak/edit', $data);
+            redirect('matkul');
+        }else{
+            $params = array('id'=>  $this->uri->segment(3));
+            $data['datamatkul'] = json_decode($this->curl->simple_get($this->API.'/matkul',$params));
+            $this->load->view('matkul/edit',$data);
         }
     }
-
-    // delete data kontak
-    function delete($id)
-    {
-        if (empty($id)) {
-            redirect('kontak');
-        } else {
-            $delete =  $this->curl->simple_delete($this->API . '/kontak', array('id' => $id), array(CURLOPT_BUFFERSIZE => 10));
-            if ($delete) {
-                $this->session->set_flashdata('hasil', 'Delete Data Berhasil');
-            } else {
-                $this->session->set_flashdata('hasil', 'Delete Data Gagal');
+    
+    // delete data matkul
+    function delete($id){
+        if(empty($id)){
+            redirect('matkul');
+        }else{
+            $delete =  $this->curl->simple_delete($this->API.'/matkul', array('id'=>$id), array(CURLOPT_BUFFERSIZE => 10)); 
+            if($delete)
+            {
+                $this->session->set_flashdata('hasil','Delete Data Berhasil');
+            }else
+            {
+               $this->session->set_flashdata('hasil','Delete Data Gagal');
             }
-            redirect('kontak');
+            redirect('matkul');
         }
     }
 }
