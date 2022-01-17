aefb9b2062a33c1df5497d949c1f11d086c64f96
Deasy Komarasary <72957690+deasyk481@users.noreply.github.com>
2022-01-17 14:47:54 +0700

    Update edit.php

--- rest_ci_client_matkul/application/views/matkul/edit.php
+++ rest_ci_client_matkul/application/views/matkul/edit.php
@@ -1,23 +1,31 @@
-<?php echo form_open('kontak/edit'); ?>
-<?php echo form_hidden('id', $datakontak[0]->id); ?>
+<?php echo form_open('matkul/edit'); ?>
+<?php echo form_hidden('id', $datamatkul[0]->id); ?>
 
 <table>
     <tr>
         <td>ID</td>
-        <td><?php echo form_input('', $datakontak[0]->id, "disabled"); ?></td>
+        <td><?php echo form_input('', $datamatkul[0]->id, "disabled"); ?></td>
+    </tr>
+    <tr>
+        <td>KODE MATKUL</td>
+        <td><?php echo form_input('kd_matkul', $datamatkul[0]->kd_matkul); ?></td>
     </tr>
     <tr>
         <td>NAMA</td>
-        <td><?php echo form_input('nama', $datakontak[0]->nama); ?></td>
+        <td><?php echo form_input('nama', $datamatkul[0]->nama); ?></td>
     </tr>
-    <tr>
-        <td>NOMOR</td>
-        <td><?php echo form_input('nomor', $datakontak[0]->nomor); ?></td>
+	<tr>
+        <td>DOSEN</td>
+        <td><?php echo form_input('dosen', $datamatkul[0]->dosen); ?></td>
+    </tr>
+	<tr>
+        <td>KELAS</td>
+        <td><?php echo form_input('kelas', $datamatkul[0]->kelas); ?></td>
     </tr>
     <tr>
         <td colspan="2">
             <?php echo form_submit('submit', 'Simpan'); ?>
-            <?php echo anchor('kontak', 'Kembali'); ?></td>
+            <?php echo anchor('matkul', 'Kembali'); ?></td>
     </tr>
 </table>
 <?php
