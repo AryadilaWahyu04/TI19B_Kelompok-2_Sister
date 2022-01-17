be09219f023801a2bcff420ecd324f04f1bebb11
fendier2k <72675305+fendier2k@users.noreply.github.com>
2022-01-17 14:10:36 +0700

    Update mahasiswa.php

--- rest_ci_mahasiswa/application/controllers/mahasiswa.php
+++ rest_ci_mahasiswa/application/controllers/mahasiswa.php
@@ -1,79 +1,70 @@
 <?php
 
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-//require APPPATH . '/libraries/REST_Controller.php';
-require('application/libraries/REST_Controller.php');
-//use Restserver\Libraries\REST_Controller;
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
 
-class Kontak extends REST_Controller
-{
+class mahasiswa extends REST_Controller {
 
-    function __construct($config = 'rest')
-    {
+    function __construct($config = 'rest') {
         parent::__construct($config);
         $this->load->database();
     }
 
-    //Menampilkan data kontak
-    function index_get()
-    {
+    function index_get() {
         $id = $this->get('id');
         if ($id == '') {
-            $kontak = $this->db->query('select * from telepon')->result();
+            $mahasiswa = $this->db->get('mahasiswa_client')->result();
         } else {
-            $kontak = $this->db->query('select * from telepon where id=' . $id)->result();
+            $this->db->where('id', $id);
+            $mahasiswa = $this->db->get('mahasiswa_client')->result();
         }
-        $this->response($kontak, 200);
-        //$this->response(array("result"=>$kontak, 200));
-
+        $this->response($mahasiswa, 200);
     }
 
-    //Mengirim atau menambah data kontak baru
-    function index_post()
-    {
-        $id = $this->post('id');
-        $nama = $this->post('nama');
-        $nomor = $this->post('nomor');
-
-
-        $kontak = $this->db->query(" insert into telepon (nama,nomor) values ('" . $nama . "','" . $nomor . "') ");
-
-        if ($kontak) {
-            $this->response(array("result" => $kontak, 200));
+    function index_post() {
+        $data = array(
+                    'id'           => $this->post('id'),
+                    'nim'          => $this->post('nim'),
+                    'nama'         => $this->post('nama'),
+                    'nomor'        => $this->post('nomor'),
+                    'alamat'       => $this->post('alamat'));
+        $insert = $this->db->insert('mahasiswa_client', $data);
+        if ($insert) {
+            $this->response($data, 200);
         } else {
             $this->response(array('status' => 'fail', 502));
         }
     }
 
-    //Memperbarui data kontak yang telah ada
-    function index_put()
-    {
+    function index_put() {
         $id = $this->put('id');
-        $nama = $this->put('nama');
-        $nomor = $this->put('nomor');
-
-
-        $kontak = $this->db->query("update telepon set nama='" . $nama . "',nomor='" . $nomor . "' where id=" . $id);
-
-        if ($kontak) {
-            $this->response(array("result" => $kontak, 200));
+        $data = array(
+                    'id'           => $this->put('id'),
+                    'nim'          => $this->put('nim'),
+                    'nama'         => $this->put('nama'),
+                    'nomor'        => $this->put('nomor'),
+                    'alamat'       => $this->put('alamat'));
+        $this->db->where('id', $id);
+        $update = $this->db->update('mahasiswa_client', $data);
+        if ($update) {
+            $this->response($data, 200);
         } else {
             $this->response(array('status' => 'fail', 502));
         }
     }
 
-    //Menghapus salah satu data kontak
-    function index_delete()
-    {
+    function index_delete() {
         $id = $this->delete('id');
-        $delete = $this->db->query('delete from telepon where id=' . $id);
+        $this->db->where('id', $id);
+        $delete = $this->db->delete('mahasiswa_client');
         if ($delete) {
-            $this->response(array('status' => 'success'), 200);
+            $this->response(array('status' => 'success'), 201);
         } else {
             $this->response(array('status' => 'fail', 502));
         }
     }
 
-    //Masukan function selanjutnya disini
-}
+	}
+?>
